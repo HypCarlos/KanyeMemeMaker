52f882d663834fa5dab0cf2174b6e5ecdce98703
Carlos <thechubbscoper@gmail.com>
2022-02-17 12:38:56 -0500

    icons added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route} from "react-router-dom";
 import "./App.css";
 import CustomText from "./components/customText/CustomText";
+
 import Main from "./components/main/Main";
 
 function App() {
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,7 +1,5 @@
 import { React, useState } from "react";
 import PHOTO from "../../img/ye.png";
-
-
 import "../main/main.css";
 import { Link } from "react-router-dom";
 
@@ -14,7 +12,7 @@ function Main() {
 
   return (
     <div className="main-container">
-      <h1 id= "title">FATHERHOLDMYNOTEPAD.COM</h1>
+      <h1 id="title">FATHERHOLDMYNOTEPAD.COM</h1>
       <div className="pic-container">
         <div className="ye-main">
           <img src={PHOTO} alt="ye" />
@@ -28,11 +26,18 @@ function Main() {
         <br />
         <br /> <br />
         <br />
-        
         <Link to={`/custom`}>
           <div class="button_slide slide_left">ADD CUSTOM TEXT</div>
           {/* <button  >Add Custom text</button> */}
         </Link>
+        <br/><br/><br/><br/>
+        
+        <a href="https://github.com/HypCarlos">
+        <img src="https://img.icons8.com/material-outlined/24/000000/github.png" alt= "Git"/>
+        </a>
+        <a href="https://twitter.com/OgPesci">
+        <img src="https://img.icons8.com/material-rounded/24/000000/twitter.png" alt= "twitter"/>
+        </a>
       </div>
     </div>
   );
