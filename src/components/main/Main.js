c0eb8d6bbe25b5de859146ff25c9d3f8c3f21238
Carlos <ccord042@fiu.edu>
2022-03-26 21:06:52 -0400

    newest

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,27 +2,12 @@ import { React } from "react";
 import "../main/main.css";
 import Screenshot from "../../Screenshot";
 
-// import CustomImage from "../customImage/Customimage";
-
-
-
-
 function Main() {
-  // const [picture, setPicture] = useState(null);
-
-  // const onChangePicture = (e) => {
-  //   setPicture(URL.createObjectURL(e.target.files[0]));
-  // };
-
-
-
   return (
-
     <div className="main-container">
-    <h1 id="title">FATHERHOLDMYNOTEPAD.COM</h1>
-    <Screenshot/>
+      <h1 id="title">FATHERHOLDMYNOTEPAD.COM</h1>
+      <Screenshot />
     </div>
-  
   );
 }
 
